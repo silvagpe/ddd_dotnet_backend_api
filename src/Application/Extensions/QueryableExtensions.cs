a48b3ff2101be422df6ad9701936481e5c567f71
Rafael Da Silva <silvagpe@gmail.com>
2025-03-27 09:14:34 -0300

    Refactor dynamic query handling by simplifying expression building and improving comparison logic

--- src/Application/Extensions/QueryableExtensions.cs
+++ src/Application/Extensions/QueryableExtensions.cs
@@ -6,40 +6,9 @@ public static class QueryableExtensions
 {
     public static IQueryable<T> WhereDynamic<T>(this IQueryable<T> query, string field, string operation, string value)
     {
-        var parameter = Expression.Parameter(typeof(T), "x");
+        var parameter = Expression.Parameter(typeof(T), "x");        
         var member = Expression.PropertyOrField(parameter, field);
-        var constant = Expression.Constant(Convert.ChangeType(value, member.Type));
-        var comparison = operation switch
-        {
-            ">=" => Expression.GreaterThanOrEqual(member, constant),
-            "<=" => Expression.LessThanOrEqual(member, constant),
-            "=" => (Expression)(value.StartsWith("*") 
-                    ? Expression.Call(
-                        Expression.Call(member, "ToLower", null),
-                        typeof(string).GetMethod("EndsWith", new[] { typeof(string) }),
-                        Expression.Constant(value.TrimStart('*').ToLower())
-                    )
-                    : value.EndsWith("*")
-                    ? Expression.Call(
-                        Expression.Call(member, "ToLower", null),
-                        typeof(string).GetMethod("StartsWith", new[] { typeof(string) }),
-                        Expression.Constant(value.TrimEnd('*').ToLower())
-                    )
-                    : Expression.Equal(
-                        Expression.Call(member, "ToLower", null),
-                        Expression.Constant(value.ToLower())
-                    )),
-            _ => throw new NotSupportedException($"Operation '{operation}' is not supported")
-        };
-
-        var lambda = Expression.Lambda<Func<T, bool>>(comparison, parameter);
-        return query.Where(lambda);
-    }
-
-    public static IQueryable<T> WhereDynamic2<T>(this IQueryable<T> query, string field, string operation, string value)
-    {
-        var comparison = ExpressionBuilder.BuildComparisonExpression<T>(field, operation, value);
-        var parameter = Expression.Parameter(typeof(T), "x");
+        var comparison = ExpressionBuilder.BuildComparisonExpression<T>(member, operation, value);    
         var lambda = Expression.Lambda<Func<T, bool>>(comparison, parameter);
         return query.Where(lambda);
     }
--- src/Application/Helpers/ExpressionBuilder.cs
+++ src/Application/Helpers/ExpressionBuilder.cs
@@ -5,57 +5,41 @@ namespace DeveloperStore.Application.Helpers;
 
 public static class ExpressionBuilder
 {
-    public static Expression BuildComparisonExpression<T>(string field, string operation, string value)
-    {
-        var parameter = Expression.Parameter(typeof(T), "x");
-        var member = Expression.PropertyOrField(parameter, field);
+    public static Expression BuildComparisonExpression<T>(MemberExpression member, string operation, string value){
+        
+        var constant = Expression.Constant(Convert.ChangeType(value, member.Type));
 
         if (operation == "=")
         {
             if (value.StartsWith("*"))
-            {
-                // Termina com (LIKE '%value')
-                var likeValue = "%" + value.TrimStart('*').ToLower();
+            {                
                 return Expression.Call(
-                    typeof(DbFunctionsExtensions).GetMethod(nameof(DbFunctionsExtensions.Like), new[] { typeof(DbFunctions), typeof(string), typeof(string) }),
-                    Expression.Constant(EF.Functions),
-                    Expression.Call(member, "ToLower", null),
-                    Expression.Constant(likeValue)
-                );
+                        Expression.Call(member, "ToLower", null),
+                        typeof(string).GetMethod("EndsWith", new[] { typeof(string) }),
+                        Expression.Constant(value.TrimStart('*').ToLower())
+                    );
             }
             else if (value.EndsWith("*"))
-            {
-                // Começa com (LIKE 'value%')
-                // var likeValue = value.TrimEnd('*').ToLower() + "%";
-                // return Expression.Call(
-                //     typeof(DbFunctionsExtensions).GetMethod(nameof(DbFunctionsExtensions.Like), new[] { typeof(DbFunctions), typeof(string), typeof(string) }),
-                //     Expression.Constant(EF.Functions),
-                //     Expression.Call(member, "ToLower", null),
-                //     Expression.Constant(likeValue)
-                // );
-
+            {                
                 return Expression.Call(
-                    Expression.Call(member, "ToLower", null),
-                    typeof(string).GetMethod("StartsWith", new[] { typeof(string) }),
-                    Expression.Constant(value.TrimEnd('*').ToLower())
+                        Expression.Call(member, "ToLower", null),
+                        typeof(string).GetMethod("StartsWith", new[] { typeof(string) }),
+                        Expression.Constant(value.TrimEnd('*').ToLower())
                 );
             }
             else
-            {
-                // Igualdade (case-insensitive)
+            {                
                 return Expression.Equal(
                     Expression.Call(member, "ToLower", null),
                     Expression.Constant(value.ToLower())
                 );
             }
-        }
-
-        var constant = Expression.Constant(Convert.ChangeType(value, member.Type));
+        }        
         return operation switch
         {
             ">=" => Expression.GreaterThanOrEqual(member, constant),
             "<=" => Expression.LessThanOrEqual(member, constant),
             _ => throw new NotSupportedException($"Operation '{operation}' is not supported")
-        };
+        };     
     }
 }

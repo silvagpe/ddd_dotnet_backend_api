1875c9e9b5b0fbf4257f4b42077d5a23c8f0cc9f
Rafael Da Silva <silvagpe@gmail.com>
2025-03-30 11:59:57 -0300

    Refactor API entry point by separating Startup configuration into its own class

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,42 +1,13 @@
-using DeveloperStore.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
-using DeveloperStore.Api.DependencyInjections;
-using DeveloperStore.Api.Middleware;
-using SharpAbp.Abp.Snowflakes;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddDbContextInjection(builder.Configuration);
-builder.Services.AddRepositoryInjection();
-builder.Services.AddMediatR();
-builder.Services.AddAutoMapperConfig();
-builder.Services.AddSnowflakeInjection();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    // app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args) =>
+        CreateHostBuilder(args).Build().Run();
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-app.UseHttpsRedirection();
-app.UseMiddleware<ErrorHandlingMiddleware>();
-
-app.MapControllers();
-
-//Automatic apply migrations.
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // Substitua pelo nome do seu DbContext
-    dbContext.Database.Migrate();
-}
-
-app.Run();
---
+++ src/Api/Startup.cs
@@ -0,0 +1,51 @@
+using DeveloperStore.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using DeveloperStore.Api.DependencyInjections;
+using DeveloperStore.Api.Middleware;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.AddSwaggerGen();
+        services.AddControllers();
+        services.AddDbContextInjection(_configuration);
+        services.AddRepositoryInjection();
+        services.AddMediatR();
+        services.AddAutoMapperConfig();
+        services.AddSnowflakeInjection();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        // Automatic apply migrations.
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.Migrate();
+        }
+    }
+}

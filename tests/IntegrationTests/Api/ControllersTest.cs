c7e50b182af299f762c53d338e1bebca53fec1df
Rafael Da Silva <silvagpe@gmail.com>
2025-03-30 15:47:40 -0300

    Add integration tests for Product creation with validation checks

--- tests/IntegrationTests/Api/ControllersTest.cs
+++ tests/IntegrationTests/Api/ControllersTest.cs
@@ -1,5 +1,8 @@
 using System.Net;
+using System.Net.Http.Json;
 using DeveloperStore.Application.Features.Branches.Dtos;
+using DeveloperStore.Application.Features.Products.Commands;
+using DeveloperStore.Application.Features.Products.Dtos;
 using DeveloperStore.Application.Models;
 using FluentAssertions;
 
@@ -84,4 +87,83 @@ public class ControllersTest : IClassFixture<TestFixture>
         items.Should().NotBeNullOrEmpty();
         items.Should().BeInDescendingOrder(b => b.Name);
     }
+
+    [Fact]
+    public async Task Post_Product_Invalid_Should_Return_ValidationError(){
+        // Arrange
+        var client = _testFixture.Client;
+        var product = new CreateProductCommand
+        {
+            Id = 0,
+            Title = "",
+            Description = "",
+            Price = 0,
+            Category = "",    
+            Rating = new RatingDto
+            {
+                Rate = 0,
+                Count = 0
+            },        
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/Products", product);        
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        responseContent.Should().NotBeNullOrEmpty();
+        responseContent.Should().Contain("Title is required.");
+        responseContent.Should().Contain("Description is required.");
+        responseContent.Should().Contain("Category is required.");
+        responseContent.Should().Contain("Price must be greater than 0.");
+    }
+    
+    [Fact]
+    public async Task Post_Product_Valid_Should_Return_ProductDto(){
+        // Arrange
+        var client = _testFixture.Client;
+        var productToPost = new CreateProductCommand
+        {            
+            Title = "Product 1",
+            Description = "Product 1 Description",
+            Price = 100,
+            Category = "category",
+            Image = "image.png",    
+            Rating = new RatingDto
+            {
+                Rate = 1.1M,
+                Count = 100
+            },        
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync<CreateProductCommand>("/api/Products", productToPost);        
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        responseContent.Should().NotBeNullOrEmpty();
+        
+        var productResult = System.Text.Json.JsonSerializer.Deserialize<ProductDto>(
+            responseContent, new System.Text.Json.JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        
+        productResult.Should().NotBeNull();
+        productResult.Id.Should().BeGreaterThan(0);
+        productResult.Title.Should().Be(productToPost.Title);   
+        productResult.Description.Should().Be(productToPost.Description);
+        productResult.Price.Should().Be(productToPost.Price);
+        productResult.Category.Should().Be(productToPost.Category);
+        productResult.Image.Should().Be(productToPost.Image);
+        productResult.Rating.Should().NotBeNull();
+        productResult.Rating.Rate.Should().Be(productToPost.Rating.Rate);
+        productResult.Rating.Count.Should().Be(productToPost.Rating.Count);
+        
+    }
+
 }
